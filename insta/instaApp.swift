bf1f41794fe65cc2b18352c7869764cec6543116
Oleksandr Pylypenko <opilipenko@gmail.com>
2025-03-10 13:21:53 +0200

    Create project

---
+++ insta/instaApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaApp.swift
+//  insta
+//
+//  Created by Oleksandr Pylypenko on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct instaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
